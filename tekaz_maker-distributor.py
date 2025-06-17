8fa2baf4f998f96a33650341e1b5eaa0f813bd01
KEJJE-CHAUNDRA <kejjetekachaundra@gmail.com>
2025-06-17 20:02:51 +0300

    Add files via upload

---
+++ tekaz_maker-distributor.py
@@ -0,0 +1,16 @@
+print ("Welcome to Tekaz mark distributor")
+mark = int(input("please enter number betweeen 1 and 100"))
+if mark< 30:
+    print("your grade is F")
+elif mark>30 and mark <40: 
+    print("your grade is E")
+elif mark>40 and mark <50: 
+    print("your grade is D")   
+elif mark>50 and mark <60: 
+    print("your grade is C")
+elif mark>60 and mark <70: 
+    print("your grade is B")
+elif mark>70 and mark <80: 
+    print("your grade is A")
+elif mark>80 and mark <101: 
+    print("your grade is A+")         
