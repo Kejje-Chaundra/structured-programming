19083e7bd90919edeb9412ed94c4625805791534
KEJJE-CHAUNDRA <kejjetekachaundra@gmail.com>
2025-06-17 19:46:46 +0300

    Add files via upload

---
+++ Tekaz-employee-distributor.py
@@ -0,0 +1,15 @@
+print ("Welcome to Tekaz employee age selector")
+age  = int(input("please enter years between 18 and 50"))
+if age> 18 and age<20 :
+ print("your to work in the loading and off loading department")
+elif age>20 and age<30 :
+  print("your to work with the packing department")
+elif age>30 and age<40 :
+   print("your to work with the delivery department")
+elif age>40 and age<50 :
+   print("your to work with the accounting department")
+elif age >50 :
+   print("you dont qualify for any job activity")
+
+
+
