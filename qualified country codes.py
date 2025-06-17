d9d03c64a5c2c45cf387dce305bce8c371dae4e1
Kejje-Chaundra <kejjetekachaundra@gmail.com>
2025-06-17 11:04:55 +0300

    Add files via upload

---
+++ qualified country codes.py
@@ -0,0 +1,20 @@
+print("your welcome to Tekaz-east african configuration app")
+code=int(input("country codes expected are +256, +257, +250, +255, +211, +254,(not expected:+234, +20, +212"))
+if code== +257 :
+ print("Burundi")
+elif code== +256 :
+ print("Uganda")
+elif code== +250 :
+ print("Rwanda")
+elif code== +255 :
+ print("Tazania")
+elif code== +211 :
+ print("Sudan")
+elif code== +254 :
+ print("Kenya")
+elif code== +234 :
+  print("not yet registered❌")
+elif code== +20 :
+ print("not yet registered❌")
+elif code== +212 :
+ print("will be activated with time⌛")
